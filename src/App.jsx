cbf0c03537ff00959ffd6044854752732a70e610
Gary Buckle <buckle.g@mac.com>
2023-05-05 06:57:59 +0000

    List component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import * as React from 'react';
+
+import './App.css';
+
+const API_ENDPOINT =
+  'http://api.geonet.org.nz/news/geonet                                                                 ';
+
+const apiReducer = (state, action) => {
+  switch (action.type) {
+    case 'API_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'API_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'API_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+
+    default:
+      throw new error(console.log('Error'));
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [apiResult, dispatchApiResult] = React.useReducer(apiReducer, {
+    data: [],
+    isLoading: false,
+    isError: false,
+  });
+  React.useEffect(() => {
+    dispatchApiResult({ type: 'API_FETCH_INIT' });
+    fetch(`${API_ENDPOINT}`)
+      .then((response) => response.json())
+      .then((result) => {
+        dispatchApiResult({ type: 'API_FETCH_SUCCESS', payload: result.hits });
+      })
+      .catch(() => {
+        dispatchApiResult({ type: 'API_FETCH_FAILURE' });
+      });
+  }, []);
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>API GEONET</h1>
+        <hr />
+        {apiResult.isError && <p>Ooops!! Unable to load data</p>}
+        {apiResult.isLoading ? (
+          '(<p>Fetching Content... hold tight!</p>'
+        ) : (
+          <h2>
+            <List list={apiResult.data} />
+          </h2>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+const List = (list) => {
+  <ul>The list of items</ul>;
+};
+const Item = () => {};
+export default App;
