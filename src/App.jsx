a2cf77f98a37371ff4f2b6a3721f5cd5c684ad9f
Gary Buckle <buckle.g@mac.com>
2023-05-16 13:35:04 +0000

    implement callback

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import * as React from 'react';
-import './App.css';
 import axios from 'axios';
 
-const API_ENDPOINT = 'http://api.geonet.org.nz/news/geonet';
-const requestOptions = {
-  headers: { 'Content-Type': 'application/json' },
-};
+import './App.css';
+
 const apiReducer = (state, action) => {
   switch (action.type) {
     case 'API_FETCH_INIT':
@@ -35,40 +32,46 @@ const apiReducer = (state, action) => {
 };
 
 function App() {
-  const [url, setUrl] = React.useState(`${API_ENDPOINT}`);
+  const endPoints = {
+    news: { url: 'http://api.geonet.org.nz/news/geonet' },
+    strong: {
+      url: 'http://api.geonet.org.nz/geonet/intensity/strong/processed/',
+    },
+    intensity: { url: 'http://api.geonet.org.nz/intensity?type=measured' },
+  };
+  const fetchUrl = 'https://hn.algolia.com/api/v1/search?query=';
   const [apiResult, dispatchApiResult] = React.useReducer(apiReducer, {
     data: [],
     isLoading: false,
     isError: false,
   });
 
-  const handleFetchAPI = React.useCallback(async () => {
+  const handleFetchApi = React.useCallback(() => {
     dispatchApiResult({ type: 'API_FETCH_INIT' });
 
-    try {
-      const result = await axios.get(url);
-
-      dispatchApiResult({
-        type: 'API_FETCH_SUCCESS',
-        payload: result.data.hits,
-      });
-    } catch {
-      dispatchApiResult({ type: 'API_FETCH_FAILURE' });
-    }
-  }, [url]);
+    axios
+      .get(fetchUrl)
+      .then((result) => {
+        dispatchApiResult({
+          type: 'API_FETCH_SUCCESS',
+          payload: result.data.hits,
+        });
+      })
+      .catch(() => dispatchApiResult({ type: 'API_FETCH_FAILURE' }));
+  }, [fetchUrl]);
 
   React.useEffect(() => {
-    handleFetchAPI();
-  }, [handleFetchAPI]);
+    handleFetchApi;
+  }, [fetchUrl]);
+
   return (
     <>
       <div>
-        <h1>API GEONET V2.0</h1>
+        <h1>API GEONET</h1>
         <hr />
-
-        {apiResult.isError && <p>Oops unable to load data</p>}
+        {apiResult.isError && <p>Ooops!! Unable to load data</p>}
         {apiResult.isLoading ? (
-          <p>Loading data ...</p>
+          '(<p>Fetching Content... hold tight!</p>'
         ) : (
           <h2>
             <List list={apiResult.data} />
@@ -78,13 +81,20 @@ function App() {
     </>
   );
 }
-
-const List = ({ list }) => <ul>{}</ul>;
-
-const Item = ({ item }) => (
+const List = (list) => {
+  {
+    console.log('The list', { list });
+    <Item key={item.objectID} item={item} />;
+  }
+};
+const Item = ({ item }) => {
   <li>
-    <p>{item.title}</p>
-  </li>
-);
-
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </li>;
+};
 export default App;
